c6906ee311b9de564d48291aaf22a9c4b50d6d10
Paul Saary <github@paulsaary.de>
2025-08-22 13:20:45 +0200

    typing for phylo

--- cstbioinfo/phylo/fasttree.py
+++ cstbioinfo/phylo/fasttree.py
@@ -6,11 +6,15 @@ from typing import List, Union
 
 from Bio import Phylo, SeqIO
 from Bio.SeqRecord import SeqRecord
-
+from Bio.Phylo import Newick
 from cstbioinfo.msa import _nuc_or_aa, msa
 
 
-class FastTree:
+class _FastTree:
+    """
+    Wrapper for FastTree to compute phylogenetic trees from aligned sequences.
+    """
+
     def __init__(self, fasttree_path: str = "fasttree"):
         self.fasttree_path = fasttree_path
         if not shutil.which(self.fasttree_path):
@@ -19,7 +23,7 @@ class FastTree:
     def compute_tree(
         self,
         aligned_sequences: Union[str, List[SeqRecord]],
-    ):
+    ) -> Newick.Tree:
         # simple subproecss call to fasttree
         with tempfile.TemporaryDirectory() as tmpdir:
             input_path = Path(tmpdir) / "aligned.fasta"
@@ -55,10 +59,27 @@ class FastTree:
 def compute_tree_fast(
     sequences: Union[list[str], list[SeqRecord]],
     fasttree_path: str = "fasttree",
-):
+) -> Newick.Tree:
+    """
+    Compute a phylogenetic tree using FastTree from a list of sequences.
+
+    Args:
+        sequences: List of sequences as strings or SeqRecord objects.
+        fasttree_path: Path to the FastTree executable.
+    Returns:
+        A Bio.Phylo tree object representing the phylogenetic tree.
+
+    Example:
+        ```python
+        from Bio import SeqIO
+        from cstbioinfo.phylo.fasttree import compute_tree_fast
+        sequences = list(SeqIO.parse("sequences.fasta", "fasta"))
+        tree = compute_tree_fast(sequences, fasttree_path="/path/to/fasttree")
+        ```
+    """
     aligned = msa(sequences)
 
     # run fasttree
-    ft = FastTree(fasttree_path=fasttree_path)
+    ft = _FastTree(fasttree_path=fasttree_path)
     tree = ft.compute_tree(aligned)
     return tree
