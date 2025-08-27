e67783b74ed5021b4888be6737608a7f9d37a47f
Paul Saary <github@paulsaary.de>
2025-08-27 11:08:58 +0200

    export the functions

--- cstbioinfo/airr/__init__.py
+++ cstbioinfo/airr/__init__.py
@@ -3,5 +3,6 @@ Tools to work with AIRR-seq data.
 """
 
 from .utils import call2gene
+from .clone import clone_shape, perfect_paired
 
-__all__ = ["call2gene"]
+__all__ = ["call2gene", "clone_shape", "perfect_paired"]
